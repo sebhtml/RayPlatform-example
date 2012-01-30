b1efccc2bc71015109b8417af62fe84457ace328
Sébastien Boisvert <seb@boisvert.info>
2012-01-30 08:24:37 -0500

    I added a shared object example.

--- Test.cpp
+++ Test.cpp
@@ -115,8 +115,6 @@ void Test::call_MY_TEST_MPI_TAG_STOP_AND_DIE(Message*message){
 
 void Test::registerPlugin(ComputeCore*core){
 
-	cout<<"Registering plugin"<<endl;
-
 	/** register the m_plugin with the core **/
 
 	m_plugin=core->allocatePluginHandle();
@@ -130,17 +128,17 @@ void Test::registerPlugin(ComputeCore*core){
 	//  - the plugin handle
 	//  - the desired value for the requested master mode handle
 
-	MY_TEST_MASTER_MODE_STEP_A=core->allocateMasterModeHandle(m_plugin,MY_TEST_MASTER_MODE_STEP_A);
+	MY_TEST_MASTER_MODE_STEP_A=core->allocateMasterModeHandle(m_plugin);
 	m_adapter_MY_TEST_MASTER_MODE_STEP_A.setObject(this);
 	core->setMasterModeObjectHandler(m_plugin,MY_TEST_MASTER_MODE_STEP_A,&m_adapter_MY_TEST_MASTER_MODE_STEP_A);
 	core->setMasterModeSymbol(m_plugin,MY_TEST_MASTER_MODE_STEP_A,"MY_TEST_MASTER_MODE_STEP_A");
 
-	MY_TEST_MASTER_MODE_STEP_B=core->allocateMasterModeHandle(m_plugin,MY_TEST_MASTER_MODE_STEP_B);
+	MY_TEST_MASTER_MODE_STEP_B=core->allocateMasterModeHandle(m_plugin);
 	m_adapter_MY_TEST_MASTER_MODE_STEP_B.setObject(this);
 	core->setMasterModeObjectHandler(m_plugin,MY_TEST_MASTER_MODE_STEP_B,&m_adapter_MY_TEST_MASTER_MODE_STEP_B);
 	core->setMasterModeSymbol(m_plugin,MY_TEST_MASTER_MODE_STEP_B,"MY_TEST_MASTER_MODE_STEP_B");
 
-	MY_TEST_MASTER_MODE_STEP_C=core->allocateMasterModeHandle(m_plugin,MY_TEST_MASTER_MODE_STEP_C);
+	MY_TEST_MASTER_MODE_STEP_C=core->allocateMasterModeHandle(m_plugin);
 	m_adapter_MY_TEST_MASTER_MODE_STEP_C.setObject(this);
 	core->setMasterModeObjectHandler(m_plugin,MY_TEST_MASTER_MODE_STEP_C,&m_adapter_MY_TEST_MASTER_MODE_STEP_C);
 	core->setMasterModeSymbol(m_plugin,MY_TEST_MASTER_MODE_STEP_C,"MY_TEST_MASTER_MODE_STEP_C");
@@ -148,24 +146,24 @@ void Test::registerPlugin(ComputeCore*core){
 	// for each slave mode, we allocate a handle 
 	// after that, we register a handler for it
 
-	MY_TEST_SLAVE_MODE_STEP_A=core->allocateSlaveModeHandle(m_plugin,MY_TEST_SLAVE_MODE_STEP_A);
+	MY_TEST_SLAVE_MODE_STEP_A=core->allocateSlaveModeHandle(m_plugin);
 	m_adapter_MY_TEST_SLAVE_MODE_STEP_A.setObject(this);
 	core->setSlaveModeObjectHandler(m_plugin,MY_TEST_SLAVE_MODE_STEP_A,&m_adapter_MY_TEST_SLAVE_MODE_STEP_A);
 	core->setSlaveModeSymbol(m_plugin,MY_TEST_SLAVE_MODE_STEP_A,"MY_TEST_SLAVE_MODE_STEP_A");
 
-	MY_TEST_SLAVE_MODE_STEP_B=core->allocateSlaveModeHandle(m_plugin,MY_TEST_SLAVE_MODE_STEP_B);
+	MY_TEST_SLAVE_MODE_STEP_B=core->allocateSlaveModeHandle(m_plugin);
 	m_adapter_MY_TEST_SLAVE_MODE_STEP_B.setObject(this);
 	core->setSlaveModeObjectHandler(m_plugin,MY_TEST_SLAVE_MODE_STEP_B,&m_adapter_MY_TEST_SLAVE_MODE_STEP_B);
 	core->setSlaveModeSymbol(m_plugin,MY_TEST_SLAVE_MODE_STEP_B,"MY_TEST_SLAVE_MODE_STEP_B");
 
-	MY_TEST_SLAVE_MODE_STEP_C=core->allocateSlaveModeHandle(m_plugin,MY_TEST_SLAVE_MODE_STEP_C);
+	MY_TEST_SLAVE_MODE_STEP_C=core->allocateSlaveModeHandle(m_plugin);
 	m_adapter_MY_TEST_SLAVE_MODE_STEP_C.setObject(this);
 	core->setSlaveModeObjectHandler(m_plugin,MY_TEST_SLAVE_MODE_STEP_C,&m_adapter_MY_TEST_SLAVE_MODE_STEP_C);
 	core->setSlaveModeSymbol(m_plugin,MY_TEST_SLAVE_MODE_STEP_C,"MY_TEST_SLAVE_MODE_STEP_C");
 
-	MY_TEST_MPI_TAG_START_STEP_A=core->allocateMessageTagHandle(m_plugin,MY_TEST_MPI_TAG_START_STEP_A);
-	MY_TEST_MPI_TAG_START_STEP_B=core->allocateMessageTagHandle(m_plugin,MY_TEST_MPI_TAG_START_STEP_B);
-	MY_TEST_MPI_TAG_START_STEP_C=core->allocateMessageTagHandle(m_plugin,MY_TEST_MPI_TAG_START_STEP_C);
+	MY_TEST_MPI_TAG_START_STEP_A=core->allocateMessageTagHandle(m_plugin);
+	MY_TEST_MPI_TAG_START_STEP_B=core->allocateMessageTagHandle(m_plugin);
+	MY_TEST_MPI_TAG_START_STEP_C=core->allocateMessageTagHandle(m_plugin);
 
 	// now, we register the order of the master modes
 	
@@ -189,19 +187,22 @@ void Test::registerPlugin(ComputeCore*core){
 
 	// configure the two message tags
 
-	MY_TEST_MPI_TAG_TIME_BOMB=core->allocateMessageTagHandle(m_plugin,MY_TEST_MPI_TAG_TIME_BOMB);
+	MY_TEST_MPI_TAG_TIME_BOMB=core->allocateMessageTagHandle(m_plugin);
 	m_adapter_MY_TEST_MPI_TAG_TIME_BOMB.setObject(this);
 	core->setMessageTagObjectHandler(m_plugin,MY_TEST_MPI_TAG_TIME_BOMB,&m_adapter_MY_TEST_MPI_TAG_TIME_BOMB);
 
-	MY_TEST_MPI_TAG_STOP_AND_DIE=core->allocateMessageTagHandle(m_plugin,MY_TEST_MPI_TAG_STOP_AND_DIE);
+	MY_TEST_MPI_TAG_STOP_AND_DIE=core->allocateMessageTagHandle(m_plugin);
 	m_adapter_MY_TEST_MPI_TAG_STOP_AND_DIE.setObject(this);
 	core->setMessageTagObjectHandler(m_plugin,MY_TEST_MPI_TAG_STOP_AND_DIE,&m_adapter_MY_TEST_MPI_TAG_STOP_AND_DIE);
 
+	/* a plugin can share any of its object with other plugins **/
+	/** other plugins have to resolve the symbol. **/
+	void*object=&m_doneA;
+	core->setObjectSymbol(m_plugin,object,"BooleanState");
 }
 
 void Test::resolveSymbols(ComputeCore*core){
 
-	cout<<"Resolving symbols"<<endl;
 	// here, we resolve symbols owned by other m_plugins
 	// but needed by the current m_plugin
 	// obviously, this is not needed here because
@@ -211,6 +212,8 @@ void Test::resolveSymbols(ComputeCore*core){
 
 	// this slave mode is registered somewhere in RayPlatform
 	RAY_SLAVE_MODE_DO_NOTHING=core->getSlaveModeFromSymbol(m_plugin,"RAY_SLAVE_MODE_DO_NOTHING");
+
+	bool*test=(bool*) core->getObjectFromSymbol(m_plugin,"BooleanState");
 }
 
 
